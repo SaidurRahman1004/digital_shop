ab882ac47d8dbcbf455f339fb3b216f318aaecd2
siyamcst <saidurrahman1004@gmail.com>
2026-02-04 01:19:40 +0600

    Search Feils and filter

---
+++ lib/widgets/hop_search_filter_bar.dart
@@ -0,0 +1,46 @@
+import 'package:digital_shop/config/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ShopSearchFilterBar extends StatelessWidget {
+  final VoidCallback onFilterTap;
+  const ShopSearchFilterBar({super.key, required this.onFilterTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search products...',
+                prefixIcon: const Icon(Iconsax.search_normal),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: AppColors.backgroundLight,
+                contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+              ),
+            ),
+          ),
+          const SizedBox(width: 12),
+          // Filter Button
+          IconButton.filled(
+            style: IconButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              padding: const EdgeInsets.all(12),
+            ),
+            onPressed: onFilterTap,
+            icon: const Icon(Iconsax.filter, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
